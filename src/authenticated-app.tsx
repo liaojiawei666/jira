286fa958b5a263c0ed94adab20f21602568ebad2
Administrator <2547456640@qq.com>
2021-11-02 21:36:54 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,25 @@
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>登出</button>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <button onClick={logout}>登出</button>
+      </Header>
       <ProjectListScreen />
     </div>
   );
 };
+
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
